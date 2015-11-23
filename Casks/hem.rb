cb721b3e21fee5b37ca2a70ac8685e8630365c0a
Andy Thompson <athompson@inviqa.com>
2015-11-23 10:04:16 +0000

    Add a cask for hem-1.0.0-dev.1

---
+++ Casks/hem.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'hem' do
+  version '1.0.0-dev.1'
+  sha256 '1af5699187a1b31d55eb6a13510f1c3b6afa5046c1ae2f8cedcf7d320e5b5600'
+
+  url "https://s3-eu-west-1.amazonaws.com/inviqa-public-tools/hem/versions/#{version}/hem-#{version}.dmg"
+  name 'Vagrant'
+  homepage 'https://github.com/inviqa/hem-gem'
+  license :mit
+
+  pkg 'hem-1.0.0-dev+20151120101714-1.pkg'
+end
