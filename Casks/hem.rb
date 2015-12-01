d14c628fb2e5dd82a14ae26b535931e74d507207
Andy Thompson <me@andytson.com>
2015-12-01 13:46:36 +0000

    Fix hem version hash

--- Casks/hem.rb
+++ Casks/hem.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'hem' do
   version '1.0.1-0.1.beta1'
-  sha256 'be6e2ca82003194bf20061da1eb52b01d81b226f9c77bee3f4d67e7424139edc'
+  sha256 '42af78a06f4e1fb9e96ac4e20ae498cdb90dc439bf5d455a8fc13d90bff8ace8'
 
   url "https://dx6pc3giz7k1r.cloudfront.net/hem/versions/#{version}/hem-#{version}.dmg"
   name 'Hem'
