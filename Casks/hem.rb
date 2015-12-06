1c30afc49e5bf77c0453d40df997ffd73ed88207
Andy Thompson <me@andytson.com>
2015-12-06 18:45:20 +0000

    Update hem cask to version 1.0.1-0.4.beta4

--- Casks/hem.rb
+++ Casks/hem.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'hem' do
-  version '1.0.1-0.2.beta2'
-  sha256 '2b630f3456bec1451e8a49e15b517272190f3bd936b57adb87ea9a0197cceb81'
+  version '1.0.1-0.4.beta4'
+  sha256 '1f8fb222ec25c53cea5e7a5f5aeb19b10cb6be61f83926f077d6a69e35d38229'
 
   url "https://dx6pc3giz7k1r.cloudfront.net/hem/versions/#{version}/hem-#{version}.dmg"
   name 'Hem'
