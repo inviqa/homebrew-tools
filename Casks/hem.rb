ad106f3bb1a66796e378e5a027d8c086f4e6d6b2
Andy Thompson <me@andytson.com>
2016-01-12 11:03:07 +0000

    Fix hem sha256 hash

--- Casks/hem.rb
+++ Casks/hem.rb
@@ -1,6 +1,6 @@
 cask 'hem' do
   version '1.0.1-0.6.beta6'
-  sha256 '2dde0882dce69d1afdfb010d14e34a7b63be929712e3637d3f118a182c8b4d88'
+  sha256 'aa044a798db415eeb61e2a5280283720de72bcc3717b6b5da89877d6a97fef89'
 
   url "https://dx6pc3giz7k1r.cloudfront.net/hem/versions/#{version}/hem-#{version}.dmg"
   name 'Hem'
