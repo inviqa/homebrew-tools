4a2fea6ba6d337e805253c8386fa82e8086fa2d2
andytson-inviqa <andytson-inviqa@users.noreply.github.com>
2016-06-10 16:58:02 +0100

    Update hem cask to 1.1.1-2

--- Casks/hem.rb
+++ Casks/hem.rb
@@ -1,6 +1,6 @@
 cask 'hem' do
-  version '1.1.1-1'
-  sha256 '3d6745b2b58c62fd90d227a4c64371f42dd7d5c697fe999e15488c35f62d5e9b'
+  version '1.1.1-2'
+  sha256 'b728e91cb1d9084ea34da6eae77c3a8e0c6cac20eaf7822353210b69ecd15cc0'
 
   url "https://dx6pc3giz7k1r.cloudfront.net/hem/versions/#{version}/hem-#{version}.dmg"
   name 'Hem'
