f076d11524f574668beb56066ad11c8f7191181a
Kieren Evans <kevans@inviqa.com>
2017-02-27 11:31:12 +0000

    Brew 1.1.10 didn't like the "name" attribute despite it being in the Formula base file.
    I also had to swap around the version and url so that ruby would embed the variable correctly.

--- Formula/cp-remote.rb
+++ Formula/cp-remote.rb
@@ -1,9 +1,8 @@
 class CpRemote < Formula
-  name "cp-remote"
   desc "Tool to create and sync code to a Continuous Pipe remote environment"
-  homepage "hhttps://docs.continuouspipe.io/guides/remote-development/"
-  url "https://continuouspipe.github.io/remote-environment-client/#{version}/darwin-amd64.gz"
+  homepage "https://docs.continuouspipe.io/guides/remote-development/"
   version '0.0.1'
+  url "https://continuouspipe.github.io/remote-environment-client/#{version}/darwin-amd64.gz"
   sha256 "b6e3d958562122e6438d528ccdd944418f93d64a040561e8cc04f24f4bd736a9"
 
   def install
