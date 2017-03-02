fe6d6607c8e12316597318c2be4cd4dc86bfb863
andytson-inviqa <andytson-inviqa@users.noreply.github.com>
2017-03-02 14:36:02 +0000

    Update cp-remote homepage url

--- Formula/cp-remote.rb
+++ Formula/cp-remote.rb
@@ -1,6 +1,6 @@
 class CpRemote < Formula
   desc "Tool to create and sync code to a Continuous Pipe remote environment"
-  homepage "https://docs.continuouspipe.io/guides/remote-development/"
+  homepage "https://docs.continuouspipe.io/remote-development/getting-started/"
   version '0.0.1'
   url "https://continuouspipe.github.io/remote-environment-client/#{version}/darwin-amd64.gz"
   sha256 "b6e3d958562122e6438d528ccdd944418f93d64a040561e8cc04f24f4bd736a9"
